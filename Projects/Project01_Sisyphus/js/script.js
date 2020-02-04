8717ad9e6ea93b0ae6d0f10001bd10a78f678cb9
Carole Chao <carolechao@hotmail.com>
2020-02-04 11:32:43 -0500

    P1: Add "score" class

--- Projects/Project01_Sisyphus/js/script.js
+++ Projects/Project01_Sisyphus/js/script.js
@@ -4,17 +4,24 @@
 let $lumberjack;
 let $rain;
 let $tree;
+// A variable to track the number of times the tree grows
+let $treeGrowth;
+let $growth = 0;
 
 // Constants to store Sound Effects
 const chopSFX = new Audio ("assets/sounds/chop.wav");
 const rainSFX = new Audio("assets/sounds/rain.wav");
 
-
+// This code will run when the document is ready
 $(document).ready(setup);
 
+// setup()
+//
+// Sets the event handlers and starts the time loop
 // This code will run when the document is ready
 function setup() {
-  $lumberjack.one('keydown',startchopSFX);
+  // Set a keydown event handler on the lumberjack image
+  $lumberjack.on('keydown',lumberjackKeydown);
 
 //
 //   $animal=$("#animal");
@@ -33,9 +40,13 @@ function setup() {
 //   });
 }
 
-function startchopSFX(e){
+// lumberjackKeydown()
+//
+// When key is down, chopSFX plays and the image is replaced by the gif.
+function lumberjackKeydown(e){
   if (e.keyCode === 32 ){ // Spacebar is pressed
   chopSFX.play();
+
   }
 }
   else if (){
