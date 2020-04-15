6ffb414384d8d6d47d6ad0a9d5d7a95ac0961a8a
Carole Chao <carolechao@hotmail.com>
2020-04-15 11:39:24 -0400

    P3: Remove excess code + Add login form
    Login form with Local Storage

--- Projects/Project03_This_time_its_personal/js/script.js
+++ Projects/Project03_This_time_its_personal/js/script.js
@@ -1,7 +1,7 @@
 /*
 
-Condiments
-Pippin Barr
+This time it's personal
+Carole Chao
 
 Chooses random words from local JSON data to fill out a sentence
 describing a condiment based on cats and rooms... weird.
@@ -13,65 +13,53 @@ https://github.com/dariusk/corpora
 
 */
 
+// Elements in the login form using Local localStorage
+const inputKey = document.getElementById("inputKey");
+const inputValue = document.getElementById("inputValue");
+const insertButton = document.getElementById("insertButton");
+
 $(document).ready(function() {
 
-  // The first thing we need to do is load the data we're going
-  // to use to get random words.
-  //
-  // For that we use jQuery's .getJSON() function, which we give
-  // the location of the file, and a function to call when the data
-  // is available...
   $.getJSON('data/data.json')
     .done(gotData)
     .fail(dataError);
+
 });
 
-// gotData (data)
 //
-// This function gets called by getJSON when the data has been loaded.
-// The data itself will be in the 'data' argument as a JavaScript object.
-function gotData(data) {
-  // Now we select random elements from the three arrays inside
-  // our JSON to get a random condiment, cat, and room. Then we add those
-  // words onto our page by setting the text of the appropriate span.
+//
+$("insertButton").onclick = function (){
+  const key= inputKey.value;
+  const value = inputValue.value;
 
-  // First the condiment
-  // Get a random condiment from the condiments array in the JSON
+  console.log(key);
+  console.log(value);
+}
+
+
+console.log(localStorage);
+
+function gotData(data) {
+
   let condiment = getRandomElement(data.condiments);
-  // Assume it's singular
   let verb = 'is';
-  // Check if the last latter of the condiment is an 's'
   if (condiment.charAt(condiment.length - 1) === 's') {
-    // If so, assume it's plural (this is a flawed assumption)
     verb = 'are';
   }
 
-  // Now the cat
   let cat = getRandomElement(data.cats);
 
-  // Same again for room
   let room = getRandomElement(data.rooms);
 
-  // Now we can construct our description with a template string
-  // We have the basic structure of a sentence and we substitute in the
-  // values we've just calculated
   let description = `${condiment} ${verb} like a ${cat} in a ${room}.`;
 
-  // Finally, we add it to the page and hey presto!
   $('body').append(description)
 }
 
-// dataError()
-//
-// Called if the JSON does not load for some reason.
-// Reports the error to the console.
 function dataError(request, text, error) {
   console.error(error);
 }
 
-// getRandomElement ()
-//
-// Returns a random element from the array provided
 function getRandomElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
