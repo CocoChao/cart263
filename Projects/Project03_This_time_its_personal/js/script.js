0f12eca8f0e682b5444265e683e56b2b9909602f
Carole Chao <carolechao@hotmail.com>
2020-04-16 17:34:24 -0400

    P3: Fixed random inspirational quote + Added random header image + Fix Login form

--- Projects/Project03_This_time_its_personal/js/script.js
+++ Projects/Project03_This_time_its_personal/js/script.js
@@ -18,9 +18,50 @@ let pageState = 1; //main page is active
 
 $(document).ready(function() {
 
+$('#two').hide();
+
   $.getJSON('data/data.json')
     .done(gotData)
     .fail(dataError);
+
+// Random Header Image in Javascript
+//
+let pictures = ['backgroundImage.jpg','backgroundImage2.jpg','backgroundImage3.jpg',
+'backgroundImage4.jpg','backgroundImage5.jpg'];
+$('#background').css({'background-image': 'url(../assets/images/'+pictures[Math.floor(Math.random()*pictures.length)]+ ')'});
+console.log(pictures);
+
+
+// gotData(data)
+//
+// Show encouraging words to the user and inspiring Oprah Winfrey quote on
+// the main page.
+function gotData(data) {
+
+  let encouraging_word = getRandomElement(data.encouraging_words);
+  // console.log(encouraging_word);
+  let subject = "You are";
+  let determinant = "a";
+  // Check if the last latter of the condiment is an 's'
+  if (encouraging_word.charAt(0) === 'a'|| encouraging_word.charAt(0) ==='e'
+  ||encouraging_word.charAt(0) === 'o'||encouraging_word.charAt(0) ==='i'
+  ||encouraging_word.charAt(0) ==='u'||encouraging_word.charAt(0) ==='u') {
+  // If word starts with vowel, the determinant is "an"
+    determinant = 'an';
+  }
+  let encouraging_words = getRandomElement(data.encouraging_words);
+  let oprahQuotes = getRandomElement(data.oprahQuotes);
+  let description = `${subject} ${determinant} ${encouraging_word} person! ${oprahQuotes}`;
+
+  $('body').append("<p id ='description'>"+description +"</p>");
+
+}
+function dataError(request, text, error) {
+  console.error(error);
+}
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
 //
 //
@@ -33,32 +74,18 @@ insertButton.onclick = function(){
   const key = inputKey.value;
   const value = inputValue.value;
 
-  console.log(key);
-  console.log(value);
-// Set any username and any password into localStorage
+  // console.log(key);
+  // console.log(value);
+// Set any username and any password into localStorage.
 localStorage.setItem("username", key);
 localStorage.setItem("password", value);
-console.log(localStorage);
-
+// When user click login, it brings it to a second page.
+alert('You are loged in.');
+pageState = 2;
+$('#two').show();
+$('#one').hide();
+$('#description').hide();
+// console.log(localStorage);
 };
 
-// gotData(data)
-//
-// Show encouraging words to the user and inspiring Oprah Winfrey quote on
-// the main page.
-function gotData(data) {
-
-  let subject = "You are a"
-  let encouraging_words = getRandomElement(data.encouraging_words);
-  let oprahQuotes = getRandomElement(data.oprahQuotes);
-  let description = `${subject} ${encouraging_words} person! ${oprahQuotes}`;
-
-  $('body').append(description)
-}
-function dataError(request, text, error) {
-  console.error(error);
-}
-function getRandomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
 });
